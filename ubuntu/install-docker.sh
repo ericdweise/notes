00df03b23ca304e48291e12cac305ccbeef3b8f9
Eric Weise <ericdweise@gmail.com>
2020-01-25 22:32:52 -0800

    Consolidate Docker installation instuctions

--- ubuntu/install-docker.sh
+++ ubuntu/install-docker.sh
@@ -1,12 +1,27 @@
+# Following steps outlined here:
+#   https://docs.docker.com/install/linux/docker-ce/ubuntu/
+# 
+# Requires sudo
+
 apt update
 apt upgrade -y
 
-apt-get install curl apt-transport-https ca-certificates software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 apt update
-apt install docker-ce
+apt install -y \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io
 
 systemctl status docker
 docker run hello-world
