44d33e61cb0697bc847c5d372136dbc3c168bda1
Eric Weise <ericdweise@gmail.com>
2020-03-04 17:24:56 -0800

    Install Docker Compose on Ubuntu

--- ubuntu/install-docker.sh
+++ ubuntu/install-docker.sh
@@ -25,3 +25,10 @@ apt install -y \
 
 systemctl status docker
 docker run hello-world
+
+
+## Installing Docker Compose
+# Following these instructions:
+#   https://docs.docker.com/compose/install/
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
